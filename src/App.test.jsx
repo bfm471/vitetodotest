9b5315e3bd9a8625bbe12e0924ef45e96cb91de4
bfm471 <bfm471@myy.haaga-helia.fi>
2023-10-14 16:33:49 +0300

    Lisätty clearTodos -toiminnallisuus + testi

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -23,3 +23,22 @@ test("add todo", () => {
     const table = screen. getByRole("table");
     expect(table).toHaveTextContent(/Go to coffee/i);
     });
+
+test("clear todos", () => {
+    render(<App />);
+    const desc = screen.getByPlaceholderText("Description");
+    const date = screen.getByPlaceholderText("Date");
+    const addbutton = screen.getByText("Add");
+    const clearbutton = screen.getByText("Clear");
+
+    fireEvent.change(desc, {target: {value: "Go to coffee"}});
+    fireEvent.change(date, {target: {value: "13.10.2023"}});
+    fireEvent.click(addbutton);
+
+    const table = screen. getByRole("table");
+    expect(table).toHaveTextContent(/go to coffee/i);
+
+    fireEvent.click(clearbutton);
+    expect(table).not.toHaveTextContent(/go to coffee/i);
+
+})
